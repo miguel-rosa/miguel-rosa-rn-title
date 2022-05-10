2437704f3393c72262f65a1f2b7d2917b0b42e33
Miguel Gonçalves <miguel.goncalves@corebiz.ag>
2022-05-10 14:42:21 -0300

    feat: test

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,18 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+type TitleProps = {
+  test?: string;
 }
+
+const Title:React.FC<TitleProps> = ({children}) => {
+  return <View style={styles.view}>{children}</View>
+}
+
+const styles = StyleSheet.create({
+  view: {
+    padding: 20,
+  }
+})
+
+export default Title;
