a9a7e95975c6b5286f2276fd291be0439944a067
Miguel Gonçalves <miguel.goncalves@corebiz.ag>
2022-05-12 11:20:59 -0300

    feat: implement title component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-miguel-rosa-rn-title';
+import { Title } from 'react-native-miguel-rosa-rn-title';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>test</Text>
+      <Title>Title</Title>
     </View>
   );
 }
