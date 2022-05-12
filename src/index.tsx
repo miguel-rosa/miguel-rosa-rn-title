3f63766aba834639bfa5e5c0e4eda32faeea599a
Miguel Gonçalves <miguel.goncalves@corebiz.ag>
2022-05-12 11:19:44 -0300

    feat: add title component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,18 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export const Title: React.FC = ({ children }) => {
+  return (
+    <View style={styles.container}>
+      <Text>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
